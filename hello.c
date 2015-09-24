1668b80d05a5e6b6b18006c1316a7c3bbd52371d
christianfredrik <christian.mentzsen@gmail.com>
2015-09-24 12:51:06 +0200

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *	hello.c
+ *
+ *	Christian Fredrik Mentzsen
+ *	christian.mentzsen@gmail.com
+ *
+ *	CS50 - pset1
+ */
+
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("hello, world\n");
+	return 0;
+}
